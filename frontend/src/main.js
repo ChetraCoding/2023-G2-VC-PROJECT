be100a9602b6ee85684279dd5556bfd0e6613f6b
PHANNITPHUOY <phannitphuoy1102@gmail.com>
2023-07-13 15:56:51 +0700

    VC-40 : Update on upload image

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import firebase from 'firebase';
 import App from './App.vue'
 import router from './router';
 import vuetify from './plugins/vuetify'
@@ -63,4 +64,14 @@ app.component('list-staff-table', ListStaffTable )
 // List Order Compoment
 app.component('chef-order-card',ChefOrderCard)
 
+const firebaseConfig = {
+  apiKey: "AIzaSyDpjEd1HdFG-DdKxnrdYHEtp6VvKNX6cY4",
+  authDomain: "vc-2023.firebaseapp.com",
+  projectId: "vc-2023",
+  storageBucket: "vc-2023.appspot.com",
+  messagingSenderId: "564664352923",
+  appId: "1:564664352923:web:6fe762ace4e6c762ad463e",
+  measurementId: "G-Z9600B42T2"
+};
+firebase.initializeApp(firebaseConfig);
 app.use(pinia).use(vuetify).use(router).mount('#app')
