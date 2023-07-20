ff2cd5fae0b00216dc5cec0b6a08d6b5871299f6
PHANNITPHUOY <phannitphuoy1102@gmail.com>
2023-07-20 13:13:10 +0700

    VC-146 : Update form of list staff

--- frontend/src/components/form/CreateStaffForm.vue
+++ frontend/src/components/form/CreateStaffForm.vue
@@ -7,105 +7,109 @@
     <v-dialog v-model="dialog" persistent width="800" no-padding>
       <!--Card-->
       <v-card
-        class="rounded-lg"
+        class="rounded-lg  bg-grey-lighten-1"
         style="max-width: 100%; max-height: 100vh; overflow-y: auto"
       >
         <!--Card title-->
         <v-card-title class="text-center bg-red-accent-2">
-          <span class="text-h6">Create new staff</span>
+          <span class="font-inter text-h6">Create new staff</span>
         </v-card-title>
         <div class="p-60">
-          <v-card-text>
-            <!--Card container-->
-            <v-container size="small">
-              <v-row class="justify-center">
-                <!--Input first name field-->
-                <v-col cols="12" md="4">
-                  <v-text-field
-                    label="First name"
-                    v-model="staff.first_name"
-                    density="compact"
-                    hide-details="auto"
-                    :error-messages="
-                      v$.first_name.$errors.map((e) => e.$message)
-                    "
-                    @input="v$.first_name.$touch"
-                    @blur="v$.first_name.$touch"
-                  ></v-text-field>
-                </v-col>
+          <!-- <v-card-text> -->
+          <!--Card container-->
+          <v-container>
+            <v-row class="d-flex px-2 mt-1 flex-column justify-center">
+              <div class="input-group gap-2">
+                <!-- <v-col cols="12" md="4"> -->
+                <v-text-field
+                  v-model="staff.first_name"
+                  class="text-black"
+                  variant="outlined"
+                  label="First name"
+                  density="compact"
+                  :error-messages="v$.first_name.$errors.map((e) => e.$message)"
+                  @input="v$.first_name.$touch"
+                  @blur="v$.first_name.$touch"
+                ></v-text-field>
+                <!-- </v-col> -->
                 <!--Input last name field-->
-                <v-col cols="12" md="4">
-                  <v-text-field
-                    label="Last name"
-                    v-model="staff.last_name"
-                    density="compact"
-                    hide-details="auto"
-                    :error-messages="
-                      v$.last_name.$errors.map((e) => e.$message)
-                    "
-                    @input="v$.last_name.$touch"
-                    @blur="v$.last_name.$touch"
-                  ></v-text-field>
-                </v-col>
+                <!-- <v-col cols="12" md="4"> -->
+                <v-text-field
+                  v-model="staff.last_name"
+                  class="text-black"
+                  label="Last name"
+                  density="compact"
+                  variant="outlined"
+                  :error-messages="v$.last_name.$errors.map((e) => e.$message)"
+                  @input="v$.last_name.$touch"
+                  @blur="v$.last_name.$touch"
+                ></v-text-field>
+                <!-- </v-col> -->
                 <!--Select gender field-->
-                <v-col cols="12" md="4">
-                  <v-select
-                    label="Gender"
-                    v-model="staff.gender"
-                    :items="['Male', 'Female', 'Other']"
-                    density="compact"
-                    hide-details="auto"
-                    :error-messages="v$.gender.$errors.map((e) => e.$message)"
-                    @input="v$.gender.$touch"
-                    @blur="v$.gender.$touch"
-                  ></v-select>
-                </v-col>
-                <!--Input email field-->
-                <v-col cols="12">
-                  <v-text-field
-                    label="Email"
-                    v-model="staff.email"
-                    density="compact"
-                    hide-details="auto"
-                    :error-messages="`${v$.email.$errors.map((e) => e.$message)}${err_email}`"
-                    @input="v$.email.$touch"
-                    @blur="v$.email.$touch"
-                  ></v-text-field>
-                </v-col>
-                <!--Input field password-->
-                <v-col cols="12">
-                  <v-text-field
-                    label="Password"
-                    v-model="staff.password"
-                    density="compact"
-                    :append-inner-icon="showPassword ? 'mdi-eye-off' : 'mdi-eye'"
-                    :type="showPassword ? 'text' : 'password'"
-                    @click:append-inner="showPassword = !showPassword"
-                    hide-details="auto"
-                    :error-messages="v$.password.$errors.map((e) => e.$message)"
-                    @input="v$.password.$touch"
-                    @blur="v$.password.$touch"
-                  ></v-text-field>
-                </v-col>
-                <!--Select role field-->
-                <v-col cols="12">
-                  <v-select
-                    label="Role"
-                    v-model="staff.role"
-                    :items="roles"
-                    :item-title="'name'"
-                    item-value="role_id"
-                    density="compact"
-                    :error-messages="v$.role.$errors.map((e) => e.$message)"
-                    @input="v$.role.$touch"
-                    @blur="v$.role.$touch"
-                  ></v-select>
-                </v-col>
-              </v-row>
-            </v-container>
-          </v-card-text>
-          <!--Action-->
-          <v-card-actions class="bg-grey-lighten-2">
+                <!-- <v-col cols="12" md="4"> -->
+                <v-select
+                  v-model="staff.gender"
+                  label="Gender"
+                  :items="['Male', 'Female', 'Other']"
+                  density="compact"
+                  class="text-black"
+                  variant="outlined"
+                  :error-messages="v$.gender.$errors.map((e) => e.$message)"
+                  @input="v$.gender.$touch"
+                  @blur="v$.gender.$touch"
+                ></v-select>
+                <!-- </v-col> -->
+              </div>
+              <!--Input email field-->
+              <!-- <v-col cols="12" class="bg-red"> -->
+              <v-text-field
+                v-model="staff.email"
+                class="mt-2 text-black"
+                label="Email"
+                density="compact"
+                variant="outlined"
+                :error-messages="`${v$.email.$errors.map(
+                  (e) => e.$message
+                )}${err_email}`"
+                @input="v$.email.$touch"
+                @blur="v$.email.$touch"
+              ></v-text-field>
+              <!-- </v-col> -->
+              <!--Input field password-->
+              <!-- <v-col cols="12"> -->
+              <v-text-field
+                v-model="staff.password"
+                label="Password"
+                density="compact"
+                class="mt-2 text-black"
+                variant="outlined"
+                :append-inner-icon="showPassword ? 'mdi-eye-off' : 'mdi-eye'"
+                :type="showPassword ? 'text' : 'password'"
+                @click:append-inner="showPassword = !showPassword"
+                :error-messages="v$.password.$errors.map((e) => e.$message)"
+                @input="v$.password.$touch"
+                @blur="v$.password.$touch"
+              ></v-text-field>
+              <!-- </v-col> -->
+              <!--Select role field-->
+              <!-- <v-col cols="12"> -->
+              <v-select
+                v-model="staff.role"
+                label="Role"
+                :items="roles"
+                :item-title="'name'"
+                item-value="role_id"
+                density="compact"
+                class="mt-2 text-black"
+                variant="outlined"
+                :error-messages="v$.role.$errors.map((e) => e.$message)"
+                @input="v$.role.$touch"
+                @blur="v$.role.$touch"
+              ></v-select>
+              <!-- </v-col> -->
+            </v-row>
+          </v-container>
+          <v-card-actions>
             <v-spacer></v-spacer>
             <!--Close button-->
             <danger-button
@@ -113,17 +117,14 @@
                 clear();
                 $emit('closeForm');
               "
-              >
-              <v-icon
-                icon="mdi-close-box-multiple"
-                color="white"
-                size="large"
-              >
+            >
+              <v-icon icon="mdi-close-box-multiple" color="white" size="large">
               </v-icon>
               Close
             </danger-button>
             <!--Save button-->
             <primary-button
+              class="mr-1"
               type="submit"
               @click="
                 () => {
@@ -131,16 +132,14 @@
                   add();
                 }
               "
-              >
-              <v-icon
-                icon="mdi-content-save-all"
-                color="white"
-                size="large"
-              >
+            >
+              <v-icon icon="mdi-content-save-all" color="white" size="large">
               </v-icon>
               Save
-              </primary-button>
+            </primary-button>
           </v-card-actions>
+          <!-- </v-card-text> -->
+          <!--Action-->
         </div>
       </v-card>
     </v-dialog>
@@ -234,3 +233,13 @@ onMounted(() => {
   getRoles();
 });
 </script>
+
+<style scoped>
+.font-inter {
+  font-family: 'Inter', sans-serif !important;
+}
+.input-group {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+}
+</style>
