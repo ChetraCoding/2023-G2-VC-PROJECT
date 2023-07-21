97d4099664a28dbd07b990214c062b26db2976fc
ChetraGithub <hongchetra22@gmail.com>
2023-07-21 10:21:59 +0700

    VC-108 : Get product report by query

--- backend/app/Http/Controllers/ProductReportController.php
+++ backend/app/Http/Controllers/ProductReportController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Order;
+use App\Models\Product;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -13,11 +15,40 @@ class ProductReportController extends Controller
      */
     public function productReport($month, $year)
     {
+        // Test ============
+        return response(
+            Order::join('order_details', 'orders.id', '=', 'order_details.order_id')
+                ->join('product_customizes', 'order_details.product_customize_id', '=', 'product_customizes.id')
+                ->join('products', 'product_customizes.product_id', '=', 'products.id')
+                ->whereYear('datetime', '=', $year)
+                ->whereMonth('datetime', '=', $month)->get()
+                // ->get()
+                ->groupBy('product_id')
+            // ->sum('order_details.quantity')
+        );
+        // =============
         // Check the user permission
         // if (!User::roleRequired('restaurant_owner')) {
         //     return response()->json(['success' => false, 'message' => "The user don't have permisstion to this route."], 403);
         // }
-        $orders = Auth::user()->store->orders;
-        return response($orders);
+        $storeId = Auth::user()->store->id;
+        // // $orders = Auth::user()->store->orders->whereDate('datetime', $selectedDate);
+        // $orders = Order::where('store_id', $storeId)
+        //     ->withCount('orderDetails')
+        //     ->with('orderDetails.productCustomize')
+        //     ->whereYear('datetime', '=', $year)
+        //     ->whereMonth('datetime', '=', $month)->get();
+        // return response($orders);
+        return response(
+            Product::with('productCustomize')
+                ->with('productCustomize.orderDetails')
+                ->with('productCustomize.orderDetails.order')
+                ->whereHas('productCustomize.orderDetails.order', function ($query) use ($month, $year) {
+                    $query->whereMonth('datetime', '=', $month)
+                        ->whereYear('datetime', '=', $year);
+                })
+                ->get()
+
+        );
     }
 }
